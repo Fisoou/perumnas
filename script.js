edf85044ce32dba556a8eea79b9342de95c2095d
Fisou <fisou69@gmail.com>
2025-09-10 02:12:32 +0300

    be

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const swiper = new Swiper(".swiper", {
-  direction: "horizontal",
-  loop: false,
-  freeMode: true,
-  slidesPerView: "auto",
   spaceBetween: 40,
+  loop: false,
+  slidesPerView: "auto",
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
 });
 
 const questions = document.querySelectorAll(".question");
@@ -11,8 +13,8 @@ const questions = document.querySelectorAll(".question");
 questions.forEach((question) => {
   question.addEventListener("click", () => {
     const answer = question.querySelector(".answer");
-    const dash = question.querySelector(".dash-vert");
+    const dash = question.querySelector("question");
     answer.classList.toggle("active");
-    dash.classList.toggle("active");
+    question.classList.toggle("active");
   });
 });
